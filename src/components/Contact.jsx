6ae2cb9b4588d3ad8271eeb99f6624b25bce87f1
Holdrulff <holdrulff@gmail.com>
2024-04-13 12:05:47 -0300

    adjustments

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -75,8 +75,9 @@ export default function Contact() {
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
         >
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
-            CONTATE-ME
+            CONTATE-ME:
           </h2>
+          <p></p>
           <nav>
             <div className=" text-white inline-flex items-center border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
               <a
