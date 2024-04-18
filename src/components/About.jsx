de433acbb477640ce987c0e20e0d76e3d2749080
Holdrulff <holdrulff@gmail.com>
2024-04-18 12:37:11 -0300

    updating projects session and about session

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,14 +4,14 @@ export default function About() {
   return (
     <section id="about" className="text-gray-400 bg-gray-900 body-font">
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
-        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <div>
-            <h1 className=" text-justify title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+        <div className="lg:flex-grow md:w-1/2 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <div className="flex justify-center items-center">
+            <h1 className="lg:text-center md:text-3xl title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
               Wesley Fernandes
             </h1>
           </div>
           <div>
-            <h1 className="text-justify title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+            <h1 className="text-left title-font md:text-2xl sm:text-2xl text-3xl mb-4 font-medium text-white">
               Estudante de Sistemas de Informação - USP
             </h1>
           </div>
@@ -49,7 +49,7 @@ export default function About() {
             </a>
           </div>
         </div>
-        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+        <div className="lg:p-16 md:w-1/2 w-5/6">
           <img
             className="object-cover object-center rounded"
             alt="hero"
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ library.add(fas); // Add the solid icon pack (includes social media icons)
 
 export default function Navbar() {
   return (
-    <header className="bg-gray-800 md:sticky top-0 z-10">
+    <header className="bg-gray-800 md:sticky top-0 z-50">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <a className="title-font font-medium text-white mb-4 md:mb-0">
           <a href="#about" className="ml-3 text-xl">
