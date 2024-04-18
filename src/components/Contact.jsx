53213eba71d7e055275dc3706dd58a56de4e3bda
Holdrulff <holdrulff@gmail.com>
2024-04-18 13:37:40 -0300

    updating handle submit function

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -73,7 +73,7 @@ export default function Contact() {
             </div>
           </div>
         </div>
-        <form netlify name="contact" method="POST" onSubmit={handleSubmit}>
+        <form netlify name="contact" onSubmit={handleSubmit}>
           <h2 className="text-white text-center sm:text-4xl text-3xl mb-1 font-medium title-font">
             CONTATE-ME:
           </h2>
